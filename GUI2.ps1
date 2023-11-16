46e6e5c582b2510c24f58208652da10f38892906
DaNapsta <bayates826@gmail.com>
2023-11-16 15:36:37 -0600

    modified:   GUI2.ps1

--- GUI2.ps1
+++ GUI2.ps1
@@ -3,18 +3,70 @@ Add-Type -AssemblyName System.Windows.Forms
 
 # Function Definitions
 function connect-azure {
-    # Your existing connect-azure function code
-    Read-host "Connect-Azure has run"
+    # Connect to the Client's Azure Environment
+    install-module -name az -allowclobber -scope currentuser
+    connect-azaccount
+    # Login via web browser
 }
 
 function deploy-baseline {
-    # Your existing deploy-baseline function code
-    read-host "Deploy-baseline has run"
+    # Build the Resource Group and assign non-user variables.
+    $resourcegroupname = "$Client-Infrastructure"
+    $vmImage = "2022-datacenter-azure-edition"
+
+    New-azresourcegroup -name "$resourcegroupname" -location $location
+
+    # Build the vNet configurations and create vNet object with two subnets (VM Network & Gateway Subnet)
+    $vnet = new-azvirtualnetwork -resourcegroupname $resourcegroupname -location $location -name "$Client-vNet" -addressprefix "$addressprefix"
+    $vmvnet = new-azvirtualnetworksubnetconfig -name "$Client-VM-Subnet" -addressprefix $vmsubnet
+    $gatewayvnet = new-azvirtualnetworksubnetconfig -name "GatewaySubnet" -addressprefix $gatewaysubnet
+
+    $vnet.subnets.add($vmvnet)
+    $vnet.subnets.add($gatewayvnet)
+    $vnet = set-azvirtualnetwork -virtualnetwork $vnet
+
+    $publicip = new-azpublicipaddress -name "$vmName-Public-IP" -resourcegroupname $resourcegroupname -location $location -sku basic -allocationmethod Dynamic
+    $nsgruleRDP = new-aznetworksecurityruleconfig -name "$vmName-RDP-Rule" -protocol Tcp -direction Inbound -priority 1000 -sourceaddressprefix * -sourceportrange * -destinationaddressprefix * -destinationportrange 3389 -access Allow
+
+    # Build out the Backup Infrastructure (Zone-redundant)
+    $vaultname = "$Client-Vault-ZRS"
+    $vault = new-azrecoveryservicesvault -name $vaultname -resourcegroupname $resourcegroupname -location $location
+    set-azrecoveryservicesvaultproperty -vaultid $vault.id
+
+    $vault1 = get-azrecoveryservicesvault -name $vaultname
+    set-azrecoveryservicesbackupproperty -vault $vault1 -backupstorageredundancy ZoneRedundant
+
+    get-azrecoveryservicesvault -name $vaultname -resourcegroupname $resourcegroupname | set-azrecoveryservicesvaultcontext
+
+    # Build the VM network interface card and firewall rules
+    $subnetid = $vnet.subnets | where-object { $_.Name -eq "$Client-VM-Subnet" }
+    $nsg = new-aznetworksecuritygroup -resourcegroupname $resourcegroupname -location $location -name "$vmName-NSG" -securityrules $nsgRuleRDP
+    $nic = new-aznetworkinterface -name "$vmName-NIC" -resourcegroupname $resourcegroupname -location $location -subnetid $subnetid.id -publicipaddressid $publicip.id -networksecuritygroupid $nsg.id
+
+    # Build the VM configuration and create the VM itself.
+    $vmConfig = new-azvmconfig -vmname $vmName -vmsize $vmSize | set-azvmoperatingsystem -Windows -Computername $vmName -credential $cred | set-azvmsourceimage -publishername "MicrosoftWindowsServer" -offer "WindowsServer" -skus $vmImage -Version "latest" | add-azvmnetworkinterface -id $nic.id
+
+    new-azvm -resourcegroupname $resourcegroupname -location $location -vm $vmConfig
+
+    # Assign the Enhanced backup policy
+    $backuppolicies = get-azrecoveryservicesbackupprotectionpolicy
+    $enhanced = $backuppolicies | where-object { $_.Name -eq "EnhancedPolicy" }
+    $vm = get-azvm -name $vmName -resourcegroupname $resourcegroupname
+    enable-azrecoveryservicesbackupprotection -resourcegroupname $resourcegroupname -name $vmName -Policy $enhanced -vaultid $vault.id
+
 }
 
 function deploy-vpn {
-    # Your existing deploy-vpn function code
-    read-host "Deploy-vpn has run"
+    # Build the VPN Gateway for the client based off the Gateway Subnet configuration from previous vNet
+    $vnet = get-azvirtualnetwork
+    $resourcegroup = $vnet.resourcegroupname
+    $vnetaddressspace = $vnet.addressspace
+    $gatewaysubnet = $vnet.subnets | where-object { $_.Name -eq "GatewaySubnet" }
+
+    $vngwpip = new-azpublicipaddress -name "$Client-VPN-Public-IP" -resourcegroupname $resourcegroup -location $location -sku basic -allocationmethod dynamic
+    $vngwipconfig = new-azvirtualnetworkgatewayipconfig -name vngwipconfig -subnetid $gatewaysubnet.id -publicipaddressid $vngwpip.id
+
+    new-azvirtualnetworkgateway -name "$Client-VPN-Gateway" -resourcegroupname $resourcegroup -location $location -ipconfigurations $vngwipconfig -gatewaytype vpn -vpntype RouteBased -gatewaysku basic
 }
 
 # Function to create input dialog
